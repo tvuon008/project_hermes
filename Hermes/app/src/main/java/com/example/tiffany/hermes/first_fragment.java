0aad59236c0c4ec52f51785dec8f9cbc83fffaa0
Tiffany Vuong <tvuon008@ucr.edu>
2017-03-02 18:10:10 -0800

    1st Upload
    This is my first upload for hermes app

---
+++ Hermes/app/src/main/java/com/example/tiffany/hermes/first_fragment.java
@@ -0,0 +1,23 @@
+package com.example.tiffany.hermes;
+
+/**
+ * Created by Tiffany on 3/2/2017.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class first_fragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout,container,false);
+        return myView;
+    }
+}
